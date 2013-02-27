08020869f8ba37fd9162ed7ae13dd8204523708a
Mário Garrido <skinie18@gmail.com>
2013-02-27 20:11:34 +0000

    ex3.c concluido

--- SCOMP/PL1/ex3.c
+++ SCOMP/PL1/ex3.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <sys/types.h>
-
-int main(void) {
+#include <unistd.h>
+int main ( void ) {
+    pid_t pid;
+    int i;
+    
+    for (i=0; i<4; i++ )
+    pid = fork();
+    
+    printf("SistComp\n");
+    
     return 0;
 }
+
+// a)
+
+/**
+ *  o
+ *  | fork()                                                                          1º
+ *  |---------------------------------------
+ *  | fork()                                | fork()                                  2º
+ *  |-------------------                    |-------------------
+ *  |                   |                   |                   |
+ *  | fork()            | fork()            | fork()            | fork()              3º
+ *  |---------          |---------          |---------          |--------- 
+ *  |fork()   |fork()   |fork()   | fork()  |fork()   |fork()   |fork()   | fork()    4º
+ *  |----     |----     |----     |----     |----     |----     |----     |----
+ *  |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |
+ */
+ 
+ // Sao criados 15 processos (o princial nao é criado, ja existe.)
+ 
+ // b) 16x
+ 
