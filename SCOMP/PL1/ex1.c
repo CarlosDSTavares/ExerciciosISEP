11f26f218e4479d82a60cf211efd72a2b4d2a025
skinie18 <skinie18@gmail.com>
2013-02-27 11:44:15 +0000

    Exericio1

---
+++ SCOMP/PL1/ex1.c
@@ -0,0 +1,42 @@
+/* prdemo04.c */
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void verificaErros(int pid);
+
+int main (void) {
+	pid_t p,a;
+	
+    p = fork();
+    verificaErros(p);
+    a = fork();
+    verificaErros(a);
+    
+    printf("Sistemas de Computadores\n");
+    return 0;
+}
+
+/** a)
+ *      o
+ *      |  fork() linha(8)
+ *      |-----------------------------
+ *      |							  |
+ *      |	fork() linha(9)			  | fork() linha(9)
+ *      |--------------- 			  |---------------
+ *      |		        |			  |              |
+ *      |		        |			  |              |
+ *      |		        |			  |              |
+ *      |		        |			  |              |
+ *   printf()       printf()       printf()       printf()  -- linha(10)
+ */
+ 
+ // b) 4x
+ 
+ // c)
+void verificaErros(int pid){
+	if(pid < 0){
+    	perror("Erro ao criar processo: ");
+    	exit(-1);
+    }
+}
