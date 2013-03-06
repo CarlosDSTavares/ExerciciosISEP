cf47dd611d4de5ffcc1e8daf8350e549df3208c5
Mário Garrido <skinie18@gmail.com>
2013-03-06 14:51:53 +0000

    fix ex8

--- SCOMP/PL1/ex8.c
+++ SCOMP/PL1/ex8.c
@@ -3,30 +3,36 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-#define NUMBER 10000
-#define PROCEC 5
+void M(char *string);
+void verify(pid_t pid);
+
+int main(void){
+	pid_t pid;
+	int i;
+	
+	for(i=0;i<2;i++){
+		pid = fork(); // priemiro fork
+		verify(pid); // verifica se o processo foi bem criado
+		if(pid > 0){ // se for pai mostra A
+			M("A\n");
+		}else{
+			M("B\n"); // se for filho mostra b
+			pid = fork(); // se for filho cria outro fork
+			verify(pid); // verifica se o processo foi bem criado
+			if(pid > 0){ // se deste segundo fork for o pai emprime A e termina
+				M("A\n");
+				exit(0);
+			}
+		}
+	}
+
+	return 0;
+}
 
 void M(char *string){
   printf(string);
 }
 
-int main(void){
-	pid_t pid;
-	int i = 0;
-	while(i < 2){
-		pid = fork();
-		if(pid > 0){
-			M("A\n");
-		}else{
-			M("B\n");
-			pid = fork();
-			if(pid > 0){
-				M("A\n");
-				exit(0);
-			}
-		}
-		i++;
-	}
-
-	exit(0);
+void verify(pid_t pid){
+	if(pid < 0) perror("ERROR: Erro ao criar processo!\n");
 }
